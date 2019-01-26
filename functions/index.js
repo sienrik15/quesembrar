28ec405abc9905ce3572570d8434dcf3a36cac39
Samuel <enrique_sam11@hotmail.com>
2019-01-26 08:37:47 -0500

    update - firebase function - crod - frestore

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,34 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const express = require("express");
 const cors = require('cors');
 const path = require('path');
 const history = require('connect-history-api-fallback');
+const CronJob = require('cron').CronJob;
+
+
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+db.settings({ timestampsInSnapshots: true });
+
+
+
+const job = new CronJob('0 0 9 * * 1-6', function() {
+//const job = new CronJob('*/5 * * * * *', function() {
+    const d = new Date();
+    const aTuringRef = db.collection('users').doc('meseta');
+    const setAlan = aTuringRef.set({
+        'first': 'Sam',
+        'middle': 'Mathison',
+        'last': 'Turing',
+        'born': d
+    });
+
+}, function() {
+    // Código a ejecutar cuando la tarea termina.
+    // Puedes pasar null para que no haga nada
+}, true);
+job.start();
 
 
 // // Create and Deploy Your First Cloud Functions
