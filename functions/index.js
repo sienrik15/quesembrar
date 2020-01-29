8815de71476eaa3bf82ab90997c7dc4b8f5ecd69
Ramon <ramon.martinez.lidon.contractor@bbva.com>
2020-01-29 14:13:13 -0500

    update -ooring

--- functions/index.js
+++ functions/index.js
@@ -250,7 +250,7 @@ app.get('/read',async (req, res) => {
         }).catch(err => {
             console.log('Error getting documents', err);
         }); */
-    let citiesRef = db.collection('lima_market').doc('watermelon').collection("prices");
+    /*let citiesRef = db.collection('lima_market').doc('watermelon').collection("prices");
     let allCities = await citiesRef.where('name', '==', "precio promedio").get()
         .then(value => {
             let res = [];
@@ -261,7 +261,7 @@ app.get('/read',async (req, res) => {
             return  res
         }).catch(err => {
             console.log('Error getting documents', err);
-        });
+        });*/
 
     /*let citiesRef = db.collection('lima_market').doc('watermelon').collection("prices").doc("avg_price");
     let allCities = await citiesRef.get()
@@ -270,6 +270,19 @@ app.get('/read',async (req, res) => {
         }).catch(err => {
             console.log('Error getting documents', err);
         });*/
+
+    let citiesRef = db.collection('agricultural_crops');
+    let allCities = await citiesRef.get()
+        .then(value => {
+            let res = [];
+            value.forEach(doc => {
+                res.push(doc.data());
+                console.log(doc.id, '=>', doc.data());
+            });
+            return  res
+        }).catch(err => {
+            console.log('Error getting documents', err);
+        });
 
     res.json(allCities);
 
